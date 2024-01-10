7bc4d1e78d039f9b48a783947f9e8848f71d7c23
isadominguez314 <113560565+isadominguez314@users.noreply.github.com>
2024-01-09 23:39:38 -0600

    testing stuff

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,3 @@
-// const express = require('express');
-// const serveIndex = require('serve-index');
-
-// const app = express();
-
-// app.use((req, res, next) => {
-//   console.log('Time: ', Date.now());
-//   next();
-// });
-
-// app.use('/request-type', (req, res, next) => {
-//   console.log('Request type: ', req.method);
-//   next();
-// });
-
-// app.use('/public', express.static('public'));
-// app.use('/public', serveIndex('public'));
-
-// app.get('/', (req, res) => {
-//   res.send('Successful response.');
-// });
-
-// app.listen(3000, () => console.log('Example app is listening on port 3000.'));
-
-
 const csv = require('csvtojson');
 const Path = require('path');
 const csvFilePath = Path.join(__dirname, "lyrics_cleaned.csv");
@@ -33,18 +8,32 @@ const port = 8080;
 
 app.use(cors());
 
-const chooseRandomSong = async () => {
-    const songsArray = await csv().fromFile(csvFilePath);
-    var randomNum = Math.floor(Math.random() * ((songsArray.length)));
-    return(songsArray[randomNum]);
-}
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
+  })
+  
+  app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+  })
+  
+  app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+  })
 
-app.get('/', async (req, res) => {
-    var randomSong = await chooseRandomSong();
-    res.send(randomSong);
-    console.log(randomSong);
-})
+// const chooseRandomSong = async () => {
+//     const songsArray = await csv().fromFile(csvFilePath);
+//     var randomNum = Math.floor(Math.random() * ((songsArray.length)));
+//     return(songsArray[randomNum]);
+// }
 
-app.listen(port, () => {
-    console.log('App listening on port ' + port);
-})
+// app.get('/', async (req, res) => {
+//     var randomSong = await chooseRandomSong();
+//     res.send(randomSong);
+//     console.log(randomSong);
+// })
+
+// app.listen(port, () => {
+//     console.log('App listening on port ' + port);
+// })
+
+module.exports = app; // Export the Express app
